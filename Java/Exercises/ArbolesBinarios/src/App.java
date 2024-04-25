328f34e201ec5fed204f12cb604c0084287929cb
Sebastian Cubides <daniel.sebastian.cubides94@gmail.com>
2024-04-24 20:05:53 -0500

    ArbolBinario completo

--- Java/Exercises/ArbolesBinarios/src/App.java
+++ Java/Exercises/ArbolesBinarios/src/App.java
@@ -1,6 +1,32 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        crearArbol();
 
     }
 
+    public static void crearArbol(){
+        ArbolBinario arbol = new ArbolBinario();
+
+        // INsertamos el primer Nodo del arbol (la raiz)
+
+        arbol.insertar(50);
+        // Insertamos otro nodo, se ubicara en la izquierda por ser menor a la raiz
+        arbol.insertar(35);
+
+        arbol.insertar(22);
+        arbol.insertar(80);
+        arbol.insertar(95);
+        arbol.insertar(99);
+        arbol.insertar(84);
+        arbol.insertar(83);
+        arbol.insertar(95);
+        arbol.insertar(80);
+
+        System.out.println("Recorrido del arbol en INORDEN: ");
+        arbol.imprimir_inorden();
+        System.out.println("Recorrido del arbol en PREORDEN: ");
+        arbol.imprimir_preorden();
+        System.out.println("Recorrido del arbol en POSTORDEN: ");
+        arbol.imprimir_postorden();
+    }
 }
--- Java/Exercises/ArbolesBinarios/src/ArbolBinario.java
+++ Java/Exercises/ArbolesBinarios/src/ArbolBinario.java
@@ -2,9 +2,6 @@ public class ArbolBinario {
 
     private Nodo raiz;
 
-    public ArbolBinario(Nodo raiz) {
-        this.raiz = raiz;
-    }
 
     public ArbolBinario() {
         this.raiz = null;
@@ -13,15 +10,15 @@ public class ArbolBinario {
     //Metodo para insertar nodos de manera recursiva:
     public Nodo insertarRecursivo(Nodo raiz, int dato){
         //Si el arbol es null, entonces creamos el primer Nodo o raiz del arbol
-        if (this.raiz == null){
-            this.raiz = new Nodo(dato);
-            return this.raiz; 
+        if (raiz == null){
+            raiz = new Nodo(dato);
+            return raiz; 
         }
         // Si no es null, entonces cargamos el arbol con valores menores a la izquierda y valores mayores a la derecha
-        if(dato < this.raiz.valor){
-            this.raiz.izquierda = insertarRecursivo(raiz.izquierda, dato); 
-        }else if(dato >= this.raiz.valor){
-            this.raiz.derecha = insertarRecursivo(raiz.derecha, dato); 
+        if(dato < raiz.valor){
+            raiz.izquierda = insertarRecursivo(raiz.izquierda, dato); 
+        }else if(dato >= raiz.valor){
+            raiz.derecha = insertarRecursivo(raiz.derecha, dato); 
         }
         return raiz;
     }
@@ -30,5 +27,47 @@ public class ArbolBinario {
         this.raiz = insertarRecursivo(raiz, numero); 
     }
 
-    
+    // Inorden = Izquierda - Raiz - Derecha 
+
+    public void imprimir_inorden_recursivo(Nodo raiz){
+        if(raiz!=null){
+            imprimir_inorden_recursivo(raiz.izquierda);
+            System.out.println(raiz.valor);
+            imprimir_inorden_recursivo(raiz.derecha);
+        }
+    }
+
+    public void imprimir_inorden(){
+        imprimir_inorden_recursivo(raiz);
+    }    
+
+    // Preorden = Raiz - Izquierda - Derecha
+
+    public void imprimir_preorden_recursivo(Nodo raiz){
+        if(raiz!=null){
+            System.out.println(raiz.valor);
+            imprimir_preorden_recursivo(raiz.izquierda);
+            imprimir_preorden_recursivo(raiz.derecha);
+        }
+    }
+
+    public void imprimir_preorden(){
+        imprimir_preorden_recursivo(raiz);
+    } 
+
+    // Postorden = Izquierda - Derecha - Raiz 
+
+    public void imprimir_postorden_recursivo(Nodo raiz){
+        if(raiz!=null){
+            imprimir_postorden_recursivo(raiz.izquierda);
+            imprimir_postorden_recursivo(raiz.derecha);
+            System.out.println(raiz.valor);
+            
+        }
+    }
+
+    public void imprimir_postorden(){
+        imprimir_postorden_recursivo(raiz);
+    } 
+
 }
